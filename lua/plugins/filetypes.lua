8c2bd1ebd1c7736ee8930e828e81b8f019c5ee63
Dexter Lui <dexlui@pm.me>
2023-06-25 20:42:55 +0800

    Add nvim-html-css plugin
    Like HTML CSS Support plugin for VSCode

--- lua/plugins/filetypes.lua
+++ lua/plugins/filetypes.lua
@@ -9,10 +9,38 @@ return {
     ft = "markdown",
   },
 
-  -- HTML
+  -- Web development (HTML, CSS, Javascript)
   {
     "rstacruz/sparkup",
     ft = "html",
+  },
+  {
+    "Jezda1337/nvim-html-css", --- Like HTML-CSS Support plugin for VSCode
+    enabled = true,
+    init = function()
+      require("html-css"):setup()
+    end,
+    dependencies = {
+      {
+        "hrsh7th/nvim-cmp",
+        opts = function(_, opts)
+          table.insert(opts.sources, {
+            name = "html-css",
+            option = {
+              max_count = {}, -- not ready yet
+              file_types = {
+                "html", -- default
+              },
+              style_sheets = {
+                "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+                -- "./style.css",
+                -- "index.css",
+              },
+            },
+          })
+        end,
+      },
+    },
   },
 }
 -- vi: fdm=indent fdl=1
