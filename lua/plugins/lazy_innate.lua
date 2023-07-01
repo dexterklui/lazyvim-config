2fee0dea3b8127e2a8093cc7f38b2caca3b9a142
Dexter Lui <dexlui@pm.me>
2023-07-01 19:37:04 +0900

    Unmap insert mode <CR> mapped by nvim-cmp
    So that I can press <CR> in insert mode to go to next line without
    needing to press <C-e> to remove the completion menu first.

--- lua/plugins/lazy_innate.lua
+++ lua/plugins/lazy_innate.lua
@@ -1,3 +1,17 @@
 return {
   { "ggandor/flit.nvim", enabled = false },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        -- ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<m-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      })
+    end,
+  },
 }
