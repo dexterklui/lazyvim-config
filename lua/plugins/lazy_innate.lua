2bc56c3f4f1f9d4d0b9f66421dadb170d6ae132a
Dexter Lui <dexlui@pm.me>
2023-07-09 23:27:59 +0800

    Fix insert mode <C-n> and <CR>
    Fix the behaviour of these mappings with plugins nvim-cmp and sparkup.
    
    nvim-cmp:
    - No longer preselect the first menu item
    - i_<CR> no longer select the first menu item even if not selected
    
    sparkup:
    - i_<C-n> works again - jumping to next input area within html code
      expanded by sparkup

--- lua/plugins/lazy_innate.lua
+++ lua/plugins/lazy_innate.lua
@@ -3,14 +3,22 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        -- ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<m-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      if opts.mapping == nil then
+        opts.mapping = {}
+      end
+      opts.mapping["<CR>"] = cmp.mapping({
+        i = function(fallback)
+          if cmp.visible() and cmp.get_active_entry() then
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+          else
+            fallback()
+          end
+        end,
+        s = cmp.mapping.confirm({ select = true }),
+        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
       })
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion = { completeopt = "menu,menuone,noselect" }
     end,
   },
   { -- auto close neo-tree on file open
