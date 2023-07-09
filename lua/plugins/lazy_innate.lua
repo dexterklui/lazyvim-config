af1e946816c8477bba0f14f81cdba55b542a676f
Dexter Lui <dexlui@pm.me>
2023-07-09 22:22:43 +0800

    neo-tree auto close on file_open

--- lua/plugins/lazy_innate.lua
+++ lua/plugins/lazy_innate.lua
@@ -1,6 +1,5 @@
 return {
-  -- { "ggandor/flit.nvim", enabled = false },
-  {
+  { -- Unmap i_<CR>
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require("cmp")
@@ -11,6 +10,21 @@ return {
         ["<C-e>"] = cmp.mapping.abort(),
         -- ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<m-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      })
+    end,
+  },
+  { -- auto close neo-tree on file open
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      if opts["event_hadnlers"] == nil then
+        opts["event_handlers"] = {}
+      end
+      table.insert(opts.event_handlers, {
+        event = "file_opened",
+        handler = function()
+          -- auto close
+          require("neo-tree").close_all()
+        end,
       })
     end,
   },
