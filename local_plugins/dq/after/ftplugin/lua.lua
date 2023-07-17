08661a8e3dee5b3ecb580dcf4b6dd0fe6b47257d
Dexter Lui <dexlui@pm.me>
2023-07-17 15:30:27 +0800

    Add mapping for lua filetype

--- local_plugins/dq/after/ftplugin/lua.lua
+++ local_plugins/dq/after/ftplugin/lua.lua
@@ -1,5 +1,20 @@
 vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
 
+-- Mini snippet
+vim.keymap.set("i", "{", function()
+  local node = vim.treesitter.get_node()
+  local nodes_active_in = { "table_constructor" }
+  if not node or not vim.tbl_contains(nodes_active_in, node:type()) then
+    if MiniPairs == nil then
+      return "{"
+    end
+    local result = MiniPairs.open("{}", "[^\\].")
+    return result == "{" and "{" or "{}<Left>"
+  end
+  return "{},<Left><Left>"
+end, { expr = true, buffer = true, noremap = true })
+
 local undo_ftplugin = "| setl ts< sw<"
+undo_ftplugin = undo_ftplugin .. "| iunmap <buffer> {"
 vim.b.undo_ftplugin = vim.b.undo_ftplugin .. undo_ftplugin
